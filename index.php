c79d71e760bb8b51d9abf91199563fbfe31bdc80
saly <faye02saly@gmail.com>
2026-06-28 09:20:11 +0000

    feat: ajout index avec menu interactif

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require "repository.php";
+require "validator.php";
+require "services.php";
+require "controller.php";
+
+do {
+    echo "\n** Menu Distributeur **\n";
+    echo "1 - Créer Wallet\n";
+    echo "2 - Faire Dépôt\n";
+    echo "3 - Faire Retrait\n";
+    echo "4 - Lister les Transactions\n";
+    echo "0 - Quitter\n";
+
+    $choix = (int)readline("Votre choix : ");
+
+    if ($choix < 0 || $choix > 4) {
+        echo "Choix invalide, veuillez réessayer\n";
+    } elseif ($choix !== 0) {
+        traiterChoix($choix);
+    }
+
+} while ($choix !== 0);
+
+echo "Au revoir !\n";
